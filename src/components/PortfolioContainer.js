47cba02cb893ae90dc1e813f7f54e980d16f7a84
NicoleD1999 <nmd1018@yahoo.com>
2022-05-21 12:14:45 -0500

    added portfolio container

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Work from './pages/Work';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+  
+    const renderPage = () => {
+      if (currentPage === 'About') {
+        return <About />;
+      }
+      if (currentPage === 'Work') {
+        return <Work />;
+      }
+      return <Contact />;
+    };
+  
+    const handlePageChange = (page) => setCurrentPage(page);
+  
+    return (
+      <div>
+        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+      </div>
+    );
+  }
